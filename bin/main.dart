cc9f8e80df66262f47320d801d0e753ca8217dc7
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-02-19 11:38:32 +0530

    code implemented for lists of unequal length using O(m+n) complexity.

--- bin/main.dart
+++ bin/main.dart
@@ -33,6 +33,15 @@ void   addNode(val) {
       currentNode.next = node; // attach node
     }
   }
+  int findSize(){
+    var currentNode = head;
+    var size=0;
+    while (currentNode != null) {
+      size=size+1;
+      currentNode = currentNode.next;
+    }
+    return size;
+  }
  void printLinkedList(){
     var currentNode = head;
     var list='';
@@ -45,86 +54,43 @@ void   addNode(val) {
 }
 
 
-int findIntersectingNode(LinkedList list1, LinkedList list2){
- var currentNode1=list1.head;
- while(currentNode1.next!=null){
-   var currentNode2=list2.head;
-   while(currentNode2!=null){
+int findIntersectingNode(LinkedList list1, LinkedList list2){ //for lists of unequal length O(m+n) complexity
+  var currentNode1=list1.head;
+  var currentNode2=list2.head;
+  var size1=list1.findSize();
+  var size2=list2.findSize();
+  var difference=(size1-size2).abs();
+ if(size1>size2){
+   while(difference>0){
+     currentNode1=currentNode1.next;
+     difference=difference-1;
+   }
+ }
+ else if(size1<size2){
+   while(difference>0){
+     currentNode2=currentNode2.next;
+     difference=difference-1;
+   }
+ }
+ while(currentNode2!=null){
      if(currentNode1.val==currentNode2.val) {
        return currentNode1.val;
      }
      else{
        currentNode2=currentNode2.next;
+       currentNode1=currentNode1.next;
      }
    }
-  currentNode1=currentNode1.next;
- }
- return null;
-}
-int findIntersectingNode2(LinkedList list1, LinkedList list2){ // O(m+n) complexity
-  Map m={};
-  var keyFound=false;
-  var currentNode1=list1.head;
-  while(currentNode1.next!=null){
-    m[currentNode1.val]=1;
-    currentNode1=currentNode1.next;
-  }
-  m[currentNode1.val]=1;
-  print(m);
-  var currentNode2=list2.head;
-  while(currentNode2.next!=null){
-    for (var k in m.keys) {
-      if(k==currentNode2.val){
-        m[k]=2;
-        keyFound=true;
-      }
-    }
-    if(!keyFound){
-      m[currentNode2.val]=1;
-    }
-    currentNode2=currentNode2.next;
-  }
-  keyFound=false;
-  for (var k in m.keys) {
-    if(k==currentNode2.val){
-      m[k]=2;
-      keyFound=true;
-    }
-  }
-  if(!keyFound){
-    m[currentNode2.val]=1;
-  }
-  print(m);
-  var answer;
-  for (var k in m.values) {
-    if(k==2){
-
-    }
-  }
-  return answer;
-}
-int findIntersectingNode3(LinkedList list1, LinkedList list2){
-  var currentNode1=list1.head;
-  var currentNode2=list2.head;
-    while(currentNode2!=null){
-      if(currentNode1.val==currentNode2.val) {
-        return currentNode1.val;
-      }
-      else{
-        currentNode2=currentNode2.next;
-        currentNode1=currentNode1.next;
-      }
-    }
-    return null;
+   return null;
 }
 void main(){
   var list1=LinkedList();
   list1.createLinkedList([3,7,8,10]);
   list1.printLinkedList();
   var list2=LinkedList();
-  list2.createLinkedList([99,1,8,10]);
+  list2.createLinkedList([99,2,1,8,10]);
   list2.printLinkedList();
-  var intersectingNode=findIntersectingNode3(list1,list2);
+  var intersectingNode=findIntersectingNode(list1,list2);
   if(intersectingNode==null){
     print('No interseting node');
   }
@@ -132,3 +98,77 @@ void main(){
     print('Intersecting Node is ='+intersectingNode.toString());
   }
 }
+
+//
+//int findIntersectingNode(LinkedList list1, LinkedList list2){
+// var currentNode1=list1.head;
+// while(currentNode1.next!=null){
+//   var currentNode2=list2.head;
+//   while(currentNode2!=null){
+//     if(currentNode1.val==currentNode2.val) {
+//       return currentNode1.val;
+//     }
+//     else{
+//       currentNode2=currentNode2.next;
+//     }
+//   }
+//  currentNode1=currentNode1.next;
+// }
+// return null;
+//}
+//int findIntersectingNode2(LinkedList list1, LinkedList list2){ // O(m+n) complexity  using hash table
+//  Map m={};
+//  var keyFound=false;
+//  var currentNode1=list1.head;
+//  while(currentNode1.next!=null){
+//    m[currentNode1.val]=1;
+//    currentNode1=currentNode1.next;
+//  }
+//  m[currentNode1.val]=1;
+//  print(m);
+//  var currentNode2=list2.head;
+//  while(currentNode2.next!=null){
+//    for (var k in m.keys) {
+//      if(k==currentNode2.val){
+//        m[k]=2;
+//        keyFound=true;
+//      }
+//    }
+//    if(!keyFound){
+//      m[currentNode2.val]=1;
+//    }
+//    currentNode2=currentNode2.next;
+//  }
+//  keyFound=false;
+//  for (var k in m.keys) {
+//    if(k==currentNode2.val){
+//      m[k]=2;
+//      keyFound=true;
+//    }
+//  }
+//  if(!keyFound){
+//    m[currentNode2.val]=1;
+//  }
+//  print(m);
+//  var answer;
+//  for (var k in m.values) {
+//    if(k==2){
+//
+//    }
+//  }
+//  return answer;
+//}
+//int findIntersectingNode3(LinkedList list1, LinkedList list2){ //for lists of equal length O(m+n) complexity
+//  var currentNode1=list1.head;
+//  var currentNode2=list2.head;
+//    while(currentNode2!=null){
+//      if(currentNode1.val==currentNode2.val) {
+//        return currentNode1.val;
+//      }
+//      else{
+//        currentNode2=currentNode2.next;
+//        currentNode1=currentNode1.next;
+//      }
+//    }
+//    return null;
+//}
--- test/day_83_morning_challenge_test.dart
+++ test/day_83_morning_challenge_test.dart
@@ -1,6 +1,36 @@
 import 'package:test/test.dart';
-
+import '../bin/main.dart';
 void main() {
-  test('calculate', () {
+  test('1', () {
+    var list1=LinkedList();
+    list1.createLinkedList([3,7,8,10]);
+    list1.printLinkedList();
+    var list2=LinkedList();
+    list2.createLinkedList([99,2,1,8,10]);
+    expect(findIntersectingNode(list1,list2),8);
+  });
+  test('2', () {
+    var list1=LinkedList();
+    list1.createLinkedList([3,7,8,10]);
+    list1.printLinkedList();
+    var list2=LinkedList();
+    list2.createLinkedList([2,1,8,10]);
+    expect(findIntersectingNode(list1,list2),8);
+  });
+  test('3', () {
+    var list1=LinkedList();
+    list1.createLinkedList([3,7,8,10]);
+    list1.printLinkedList();
+    var list2=LinkedList();
+    list2.createLinkedList([2,1,9,10]);
+    expect(findIntersectingNode(list1,list2),10);
+  });
+  test('4', () {
+    var list1=LinkedList();
+    list1.createLinkedList([3,7,8,10]);
+    list1.printLinkedList();
+    var list2=LinkedList();
+    list2.createLinkedList([2,1,9,12]);
+    expect(findIntersectingNode(list1,list2),null);
   });
 }
